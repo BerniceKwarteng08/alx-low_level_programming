cefbfe21731dbd3bfeda3fb889b755c2a28b1622
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-15 21:01:10 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Program that prints a lin with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar,the outcome is a piece of art, \n")
+return (0);
+}
