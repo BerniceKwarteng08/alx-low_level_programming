bcc3cf6a8528c902d705d6961aeb83476055c293
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-15 20:36:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like a multiligual puzzle");
+return (0);
+}
