e283af8bb07763b03feb8e0193c6e359089f625c
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-19 20:27:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - reversal of alphabet
+ * Return: 0 (good to go)
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 'z';
+	while
+		(k <= 'a')
+		{
+			putchar(k);
+			k--;
+		}
+	putchar('\n');
+	return (0);
+}
