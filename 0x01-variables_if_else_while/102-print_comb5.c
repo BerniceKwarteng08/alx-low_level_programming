0793c2113ba0ece3bc741dda4e41ecb1760f4236
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-19 22:34:44 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - combinations of t
+ * main - combinations of two-digits
+ * Return: 0 (good to go)
+ */
+int main(void)
+{
+	int a, g;
+
+	for (a = 0; a <100; a++)
+	{
+		for (g = 0; g <100; g++)
+		{
+			if (a < g)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((g / 10) + 48);
+				putchar((g % 10) + 48);
+				if (a != 98 || g != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
