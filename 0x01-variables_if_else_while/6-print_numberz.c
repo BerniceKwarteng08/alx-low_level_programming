aa9e2c54cbe7bf38d7685d8e4035cd5e890348bc
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-19 20:17:33 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - single digit base
+ * Return: 0 (good to go)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10)
+		{
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
