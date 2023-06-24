4609aa2f9ab3d40f6cf1e03feb738bba9df947c3
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-23 18:47:15 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * _isupper - check for upper cases
+ * @c: number to be checked
+ * Return: 1 (upper case) or 0 (anthing otherwise)
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
