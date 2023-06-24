6f4f88a8c11f07881b19218cbfdce311b4518336
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-23 21:36:37 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle of squres
+ * @size: size of square triangle
+ * Return: 0 (good to go)
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b >1; b--)
+			{
+				_putchar(32);
+			}
+			for (c = 0; c <= a; c++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
