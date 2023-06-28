feea97a8fb773037401dafd60fb2fb188a91bdca
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-27 17:28:45 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function thst print string
+ * @str: string to be printed
+ * Return: empty
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+			{
+			_putchar(*str);
+			}
+			_putchar('\n');
+}
