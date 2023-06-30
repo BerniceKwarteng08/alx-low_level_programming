8056ca75ea7f2d5c3e25be6553f354f63ef4e495
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-06-30 06:05:22 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - functions that capitalizes string
+ * @str: string to be treated
+ * Return: pointer to changed string
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				index == 0)
+			str[index] -= 32;
+	}
+	return (str);
+}
