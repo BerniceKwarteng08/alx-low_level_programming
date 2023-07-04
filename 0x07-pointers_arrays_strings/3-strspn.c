8cb06e65a1bbba9e170fab652ad86f9aaf43654c
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-07-03 17:47:36 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,19 +8,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int i = 0;
+	int r;
 
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		check = 0
-
-			for (n = 0; accept[n] != '\0'; n++)
-			{
-				if (accept[n] == s[i])
-				{
-					value++;
-					check = 1;
-				}
-			}
+	for (r = 0; accept[r]; r++)
+	{
+		if (*s == accept[r])
+		{
+			i++;
+			break;
+		}
+		else if (accept[r + 1] == '\0')
+			return (i);
+	}
+	s++;
+	}
+	return (n);
+}
