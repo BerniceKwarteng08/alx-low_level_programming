59683cf3dd9950550f0ee3e8d77e91f5c1aa08a3
BerniceKwarteng08 <bkwarteng105@gmail.com>
2023-07-19 08:39:28 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints name using pointer
+ * @name: string to be checked
+ * @f: pointer to function
+ * Return: 0 (good to go)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == Null)
+		return;
+	f(name);
+}
